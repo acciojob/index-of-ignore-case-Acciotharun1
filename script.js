96f5c8a252ba5e8cc19c58b06b829d02ee23fbb0
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-10-01 10:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if(s1===""){
+        return -1;
+    }
+    if(s2===""){
+        return  0;
+    }
+    if(s1!="" && s2!=""){
+       let lowerstr= s1.toLowerCase();
+       let lowersubstr=s2.toLowerCase();
+       let index=lowerstr.indexOf(lowersubstr);
+       return index;
+    }
 }
 
 // Please do not change the code below
